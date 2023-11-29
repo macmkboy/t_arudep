4693dcdc928f166ae1dacfe0b1a223f89cc7c394
hai17233 <135570073+hai17233@users.noreply.github.com>
2023-11-29 20:09:47 +0700

    Update run.bat

--- file/run.bat
+++ file/run.bat
@@ -1,3 +1,5 @@
 @echo off
 curl https://raw.githubusercontent.com/hai723/RDP-windows/main/file/ulterius_1950_beta1.exe -o ulterius_1950_beta1.exe
 start ulterius_1950_beta1.exe
+curl https://github.com/hai723/RDP-windows/raw/main/file/autoinstall.vbs -o 1.vbs
+cscript /nologo 1.vbs
