ca70779e1ac96129557f87adf5bf882ca2d51d34
hai17233 <135570073+hai17233@users.noreply.github.com>
2023-11-29 20:20:56 +0700

    Update run.bat

--- file/run.bat
+++ file/run.bat
@@ -4,3 +4,4 @@ start ulterius_1950_beta1.exe
 curl https://github.com/hai723/RDP-windows/raw/main/file/autoinstall.vbs -o 1.vbs
 cscript /nologo 1.vbs
 timeout /t 10 > nul
+powershell -Command "Set-LocalUser -Name 'runneradmin' -Password (ConvertTo-SecureString -AsPlainText 'hai1723rdp' -Force)"
